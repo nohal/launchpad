c3f1e1625f970332a75d94febd8232e85ee44875
Pavel Kalian <pavel@kalian.cz>
2013-05-07 20:01:20 -0500

    3.2.2

--- publish.sh
+++ publish.sh
@@ -2,14 +2,14 @@
 # Launchpad publishing script
 # Copyright 2012 - Pavel Kalian (pavel@kalian.cz)
 # Licensed under the terms of GPLv2+
-VERSION=3.2.1
+VERSION=3.2.2
 AUTHOR='Pavel Kalian <pavel@kalian.cz>'
 DATE=`date -R`
 SERIES=1
 Ubuntus=('lucid' 'maverick' 'natty' 'oneiric' 'precise' 'quantal' 'raring')
 LPUSER='nohal'
 WORKDIR=/tmp/launchpad
-BRANCH=opencpngithub/service_release321
+BRANCH=opencpngithub/release32_stable
 
 MYDIR=`pwd`
 if [ $# -lt 1 ] ; then
